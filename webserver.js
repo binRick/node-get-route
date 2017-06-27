43923db4ef644a0c1b732ecb5faceafa3fb549a6
rick <rick@ricks-Air.cfl.rr.com>
2017-06-26 20:15:47 -0400

    test

--- webserver.js
+++ webserver.js
@@ -1,19 +1,41 @@
 #!/usr/bin/env node
 
 var express = require('express'),
-    snmp = require('snmp-native'),
-    app = express(),
-    config = require('./config'),
     c = require('chalk'),
-    NodeCache = require('node-cache'),
-    myCache = new NodeCache();
+    async = require('async'),
+    _ = require('underscore'),
+    snmp = require("net-snmp"),
+    config = require('./config'),
+    app = express(),
+    routerSessions = [];
 
-app.get('/route/:ip', function(req, res) {
-	var host='car1';
-	var session = new snmp.Session(config.hosts[host]);
-	res.send(req.params.ip);
+_.each(_.keys(config.hosts), function(h) {
+    routerSessions.push({
+        router: h,
+        session: new snmp.Session(config.hosts[h].host, config.hosts[h].community)
+    });
 });
 
-app.listen(config.port, function() {
+app.get('/route/:ip', function(req, res) {
+    var oids = ['1.3.6.1.2.1.4.21.1.7.' + req.params.ip];
+    async.mapLimit(routerSessions, 5, function(routerSession, _cb) {
+            routerSession.session.get(oids, function(err, snmpResults) {
+                if (err)
+                    var route = null;
+                else
+                    var route = snmpResults[0].value.toString();
+                _cb(null, {
+                    router: routerSession.router,
+                    route: route
+                });
+            });
+        },
+        function(err, Routes) {
+            if (err) throw err;
+            res.json(Routes);
+        });
+});
+
+app.listen(config.port, '0.0.0.0', function() {
     console.log(c.white.bold('Webserver listening on port'), c.red(config.port));
 });
